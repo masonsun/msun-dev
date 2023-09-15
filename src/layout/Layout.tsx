08af373a9b95e671aa5f4d22f235eaf1d616b3d0
masonsun <11341267+masonsun@users.noreply.github.com>
2023-09-15 07:34:48 -0700

    Copyright and common theme (#3)

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,47 +1,61 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import { Box, Grid, Typography } from "@mui/material";
+import { Box, Grid, useTheme } from "@mui/material";
 import CssBaseline from "@mui/material/CssBaseline";
 
 import CloudsCanvas from "../components/objects/CloudsCanvas";
 import GlassBox from "../components/objects/GlassBox";
-
+import AboutMe from "../components/AboutMe";
+import Copyright from "../components/Copyright";
 import HeroText from "../components/HeroText";
 import DrawerAppBar from "../components/DrawerAppBar";
 import BackToTopButton from "../components/BackToTopButton";
-import AboutMe from "../components/AboutMe";
 import NextSectionButton from "../components/NextSectionButton";
 
 // @ts-ignore
 const BaseLayout = ({ children }) => {
+  const theme = useTheme();
+
   return (
-    <>
+    <Box component="div" sx={{ backgroundColor: theme.palette.primary.main }}>
       <CssBaseline />
 
-      {/* Hero */} 
+      {/* Hero */}
       <Box component="div" id="page-top">
         <DrawerAppBar />
-        <Grid container sx={{ position: "static", minWidth: "100%", height: "100vh" }}>
-            <CloudsCanvas />
+        <Grid
+          container
+          sx={{ position: "static", minWidth: "100%", height: "100vh" }}
+        >
+          <CloudsCanvas />
         </Grid>
         <Grid container>
-          <Grid item xs={12} sx={{ position: "absolute", top: "50%", left: "50%", width: "400px" }}>
+          <Grid
+            item
+            xs={12}
+            sx={{
+              position: "absolute",
+              top: "50%",
+              left: "50%",
+              width: "400px",
+            }}
+          >
             <HeroText />
           </Grid>
         </Grid>
       </Box>
 
-      {/* Back-to-top fab button */}
+      {/* Navigation buttons */}
       <NextSectionButton elementId="about-me" />
       <BackToTopButton elementId="page-top" />
 
       {/* About Me */}
-      <Box component="div" id="about-me" sx={{ backgroundColor: "#e1f5fe"}}>
+      <Box component="div" id="about-me">
         <AboutMe />
       </Box>
 
-      <Box component="div" sx={{ backgroundColor: "#e1f5fe"}}>
+      <Box component="div">
         <Grid container sx={{ position: "static", height: "100vh" }}>
           <Grid item xs={12}>
             <GlassBox />
@@ -49,8 +63,11 @@ const BaseLayout = ({ children }) => {
         </Grid>
       </Box>
 
-
-    </>
+      {/* Copyright */}
+      <Box component="div">
+        <Copyright />
+      </Box>
+    </Box>
   );
 };
 
