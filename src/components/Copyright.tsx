1b1a2d137b03cc67bcb88ece608f05ae5f086bb0
Mason Sun <11341267+masonsun@users.noreply.github.com>
2023-09-15 07:28:36 -0700

    Copyright component

--- src/components/Copyright.tsx
+++ src/components/Copyright.tsx
@@ -1,6 +1,23 @@
-export default function Copyright() {
-    return (
-        <>
-        </>
-    )
-}
+import React from "react";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import { useTheme } from "@mui/material";
+
+import Content from "../config/content.json";
+
+const Copyright = () => {
+  const theme = useTheme();
+  const { copyright } = Content;
+
+  return (
+    <Grid container sx={{ paddingX: 10, paddingY: 3 }}>
+      <Grid container item xs={12} md={12} justifyContent="left">
+        <Typography variant="body2" color={theme.palette.text.secondary}>
+          &copy; {new Date().getFullYear()} {copyright}
+        </Typography>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default Copyright;
